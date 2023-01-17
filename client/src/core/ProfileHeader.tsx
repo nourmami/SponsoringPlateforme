d02d37e00e73b28441f014c8ef9a3e1985e0cddb
Nie-Mand <chill.out.x19@gmail.com>
2023-01-17 18:59:47 +0100

    handled undefined pictures

---
+++ client/src/core/ProfileHeader.tsx
@@ -0,0 +1,50 @@
+export default function ProfileHeader(props: Props) {
+  return (
+    <div>
+      <img
+        src={props.coverPicture || '/art/cover.png'}
+        alt="cover photo"
+        className="h-56 w-full object-cover rounded-2xl"
+      />
+      <div className="px-4 lg:px-16 flex">
+        <img
+          src={
+            props.profilePicture ||
+            'https://api.dicebear.com/5.x/lorelei-neutral/svg?seed=' +
+              props.name +
+              '&radius=50'
+          }
+          alt="profile picture"
+          className={`ring-[5px] ring-background relative rounded-full -top-14 w-48 h-48 object-cover ${
+            !props.profilePicture && 'border'
+          }`}
+        />
+        <div className="flex-1 py-4 flex flex-col lg:flex-row justify-between">
+          <div className="pl-4">
+            <div className="flex flex-row justify-between lg:justify-start lg:flex-col space-y-3">
+              <div>
+                <h1 className="font-bold text-xl">{props.name}</h1>
+                <h2 className="font-medium text-xs">{props.title}</h2>
+              </div>
+              <div className="text-secondary-600 flex items-center space-x-1">
+                <img src="/icons/sponsors.svg" alt="sponsors" />
+                <span className="font-medium">{props.sponsorsCount || 0}</span>
+              </div>
+            </div>
+          </div>
+
+          <div>{props.actions}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+interface Props {
+  coverPicture?: string
+  profilePicture?: string
+  name: string
+  title: string
+  sponsorsCount?: number
+  actions?: React.ReactNode
+}
--- client/src/routes/profile/index.tsx
+++ client/src/routes/profile/index.tsx
@@ -1,4 +1,5 @@
 import FollowBox from '~/core/FollowBox'
+import ProfileHeader from '~/core/ProfileHeader'
 import SponsorBox from '~/core/SponsorBox'
 import { Button, Container } from '~/ui'
 
@@ -7,43 +8,22 @@ export default function Index() {
     <div className="py-10">
       <Container className="grid grid-cols-4 gap-10">
         <div className="col-span-4 xl:col-span-3">
-          <div>
-            <img
-              src="/fake/cover.png"
-              alt="cover photo"
-              className="h-56 w-full object-cover rounded-2xl"
-            />
-            <div className="px-4 lg:px-16 flex">
-              <img
-                src="/fake/profile.png"
-                alt="profile picture"
-                className="relative -top-14 w-48 h-48 object-cover rounded-full border-4 border-background"
-              />
-              <div className="flex-1 py-4 flex flex-col lg:flex-row justify-between">
-                <div className="pl-4">
-                  <div className="flex flex-row justify-between lg:justify-start lg:flex-col space-y-3">
-                    <div>
-                      <h1 className="font-bold text-xl">John Doe</h1>
-                      <h2 className="font-medium text-xs">Designer & CEO</h2>
-                    </div>
-                    <div className="text-secondary-600 flex items-center space-x-1">
-                      <img src="/icons/sponsors.svg" alt="sponsors" />
-                      <span className="font-medium">54</span>
-                    </div>
-                  </div>
+          <ProfileHeader
+            profilePicture="/fake/profile.png"
+            name="John Doe"
+            title="Designer & CEO"
+            sponsorsCount={54}
+            actions={
+              <div className="flex space-x-2 justify-end">
+                <div>
+                  <FollowBox does />
                 </div>
-
-                <div className="flex space-x-2 justify-end">
-                  <div>
-                    <FollowBox does />
-                  </div>
-                  <div>
-                    <SponsorBox does={false} />
-                  </div>
+                <div>
+                  <SponsorBox does={false} />
                 </div>
               </div>
-            </div>
-          </div>
+            }
+          />
         </div>
         <div>
           <h1>somelist</h1>
